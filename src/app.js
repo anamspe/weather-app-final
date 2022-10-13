886a537978ded9ad091ed5e8ca90a3d385bf753e
anamspe <anmeuri@gmail.com>
2022-10-12 19:55:51 -0700

    Update Date and Time info

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+// Time data
+
+let today = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[today.getDay()];
+
+let hours = today.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = today.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let time = `${hours}:${minutes}`;
+
+let currentDayTime = document.querySelector("#day-and-time");
+currentDayTime.innerHTML = `${day} | ${time}`;
+
 function displayTemperature(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#temperature");
