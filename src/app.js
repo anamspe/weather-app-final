65b2fc3bdf791b9b2a490543784091491d1017be
anamspe <anmeuri@gmail.com>
2022-10-13 15:16:51 -0700

    Added real data to main temperature icon

--- src/app.js
+++ src/app.js
@@ -37,12 +37,17 @@ function displayTemperature(response) {
   let minTempElement = document.querySelector("#min-temp");
   let maxTempElement = document.querySelector("#max-temp");
   let tempDescriptionElement = document.querySelector(".temp-description");
+  let iconElement = document.querySelector(".main-temp-img");
+
   temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}º`;
   cityElement.innerHTML = response.data.name;
   countryElement.innerHTML = response.data.sys.country;
   minTempElement.innerHTML = `${Math.round(response.data.main.temp_min)}º`;
   maxTempElement.innerHTML = `${Math.round(response.data.main.temp_max)}º`;
   tempDescriptionElement.innerHTML = response.data.weather[0].description;
+  let icon = response.data.weather[0].icon;
+  iconElement.setAttribute("src", `images/${icon}.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = `6914b599dae34153d2187249330170b1`;
@@ -50,3 +55,5 @@ let city = `Vancouver`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+// Search and Home button Temperature Data
